4b5c68657a51d14f1763436d91beb532961d227c
Edu <edu_2015@live.com>
2022-09-02 18:39:33 -0300

    feature-list-card

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <base-list-card-high-lights />
+  </div>
+</template>
+
+<script>
+import BaseListCardHighLights from './components/BaseListCardHighLights.vue';
+import './assets/main.css';
+export default {
+  name: 'App',
+  components: {
+    BaseListCardHighLights
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BaseListCardHighLights.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card-list">
+    <base-card-high-lights />
+    <base-card-high-lights />
+    <base-card-high-lights />
+    <base-card-high-lights />
+    <base-card-high-lights />
+  </div>
+</template>
+
+<script>
+import BaseCardHighLights from './BaseCardHighLights.vue';
+export default {
+  components: {
+      BaseCardHighLights
+    }
+}
+</script>
+
+<style>
+  .card-list {
+    display: flex;
+    flex-direction: row;
+    gap:26px;
+  }
+</style>
